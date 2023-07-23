22f8ad7e8f802bdd45de6763b56c9f96c0a4644a
jimmyshoubaki <jimmyshoubaki@gmail.com>
2023-07-23 15:20:50 -0500

    updated user hook

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -4,7 +4,7 @@ import {
   useSessionContext,
   useUser as useSupaUser,
 } from "@supabase/auth-helpers-react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 type UserContextType = {
   accessToken: string | null;
@@ -68,4 +68,23 @@ export const MyUserContextProvider = (props: Props) => {
       setSubscription(null);
     }
   }, [user, isLoadingUser]);
+
+  const value = {
+    accessToken,
+    user,
+    userDetails,
+    isLoading: isLoadingUser || isLoadingData,
+    subscription,
+  };
+
+  return <UserContext.Provider value={value} {...props} />;
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("useUser must be used withing a MyUserContextProvider");
+  }
+
+  return context;
 };
