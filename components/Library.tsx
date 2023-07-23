0f614b16bf35620d6899e0a3d41e27e1b295ad73
jimmyshoubaki <jimmyshoubaki@gmail.com>
2023-07-22 19:26:18 -0500

    Initial commit

---
+++ components/Library.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const Library = () => {
+    const onClick = () =>{
+        //Handle upload later
+    }
+    return (  
+        <div>Library</div>
+    );
+}
+ 
+export default Library;
