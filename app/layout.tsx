9efb8d2ac9f1fcc5437c2316c83cec83fcb12c35
jimmyshoubaki <jimmyshoubaki@gmail.com>
2023-08-13 22:32:09 -0500

    added Logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ const font = Figtree({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Cloneify",
   description: "Listen to music!",
+  icons: "/images/Spot_icon.svg"
 };
 
 export const revalidate=0;
